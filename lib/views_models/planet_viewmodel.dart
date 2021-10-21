81da19b3fc78b4ffe2c0f94b6d2fdb8cab180af8
Tiago Kazuo Lino <tiagokazuolino@hotmail.com>
2021-10-20 22:32:58 -0300

    Create model

--- lib/views_models/planet_viewmodel.dart
+++ lib/views_models/planet_viewmodel.dart
@@ -7,12 +7,16 @@ class PlanetViewModel = _PlanetViewModelBase with _$PlanetViewModel;
 abstract class _PlanetViewModelBase with Store {
   @observable
   List<ItemViewModel> listItens = [
-    ItemViewModel(nome: "teste", size: 5.0),
-    ItemViewModel(nome: "teste", size: 5.0),
+    ItemViewModel(nome: "a", size: 5.0),
+    ItemViewModel(nome: "b", size: 5.0),
     ItemViewModel(nome: "teste", size: 5.0),
     ItemViewModel(nome: "teste", size: 5.0),
     ItemViewModel(nome: "teste", size: 5.0),
     ItemViewModel(nome: "teste", size: 5.0),
     ItemViewModel(nome: "teste", size: 5.0),
   ];
+  @action
+  additem(ItemViewModel model) {
+    listItens.add(model);
+  }
 }
